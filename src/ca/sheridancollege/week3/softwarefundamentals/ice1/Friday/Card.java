c09fe96c1f18c6f31603753874f3c790b54b51eb
Hoora77 <houra.tavana@gmail.com>
2021-02-12 23:48:00 +0500

    Initial Commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
@@ -7,7 +7,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Hoora Tavana
  */
 public class Card {
 
--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,12 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Hoora Taavana
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+        Card card = new Card();
     }
     
 }
